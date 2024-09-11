4d585d7a973b657080f03b11340042cdca4fd588
Aneel Grewal <aneel.grewal@hotmail.com>
2024-09-11 23:56:32 +0200

    extension method created to map game entity to Dto

--- Endpoints/GameEndpoints.cs
+++ Endpoints/GameEndpoints.cs
@@ -17,7 +17,7 @@ public static class GameEndpoints
             .WithParameterValidation(); //inforce validations to all endpoints using nuget package (MinimalApis.Extensions)
 
         //GET request to get all games
-        group.MapGet("/", (IGamesRepository repository) => repository.GetAll());
+        group.MapGet("/", (IGamesRepository repository) => repository.GetAll().Select(game => game.AsDto()));
 
         //GET request to get game by id
         //group.MapGet("/games/{id}", (int id) => games.Find(game => game.Id == id));
@@ -32,21 +32,31 @@ public static class GameEndpoints
             }
             return Results.Ok(game); */
             //refactoring above code in one line
-            return game is not null ? Results.Ok(game) : Results.NotFound();
+            return game is not null ? Results.Ok(game.AsDto()) : Results.NotFound();
 
         }).WithName(GetGameEndpointName);
 
 
         //POST request to create resource
-        group.MapPost("/", (Game game, IGamesRepository repository) =>
+        group.MapPost("/", (CreateGameDto gameDto, IGamesRepository repository) =>
         {
+
+            Game game = new()
+            {
+                Name = gameDto.Name,
+                Genre = gameDto.Genre,
+                Price = gameDto.Price,
+                ReleaseDate = gameDto.ReleaseDate,
+                ImageUrl = gameDto.ImageUrl
+            };
+
             repository.Create(game);
 
             return Results.CreatedAtRoute(GetGameEndpointName, new { id = game.Id }, game);
         });
 
         //PUT request to update record
-        group.MapPut("/{id}", (int id, Game updatedGame, IGamesRepository repository) =>
+        group.MapPut("/{id}", (int id, UpdateGameDto updatedGameDto, IGamesRepository repository) =>
         {
             Game? existingGame = repository.Get(id);
 
@@ -55,11 +65,11 @@ public static class GameEndpoints
                 return Results.NotFound();
             }
 
-            existingGame.Name = updatedGame.Name;
-            existingGame.Genre = updatedGame.Genre;
-            existingGame.Price = updatedGame.Price;
-            existingGame.ReleaseDate = updatedGame.ReleaseDate;
-            existingGame.ImageUrl = updatedGame.ImageUrl;
+            existingGame.Name = updatedGameDto.Name;
+            existingGame.Genre = updatedGameDto.Genre;
+            existingGame.Price = updatedGameDto.Price;
+            existingGame.ReleaseDate = updatedGameDto.ReleaseDate;
+            existingGame.ImageUrl = updatedGameDto.ImageUrl;
 
             repository.Update(existingGame);
 
