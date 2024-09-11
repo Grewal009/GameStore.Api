579c04486dd042e271233348230631b4b36b4b78
Aneel Grewal <aneel.grewal@hotmail.com>
2024-09-11 23:45:13 +0200

    created Dtos

---
+++ Dtos.cs
@@ -0,0 +1,28 @@
+
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+public record GameDto(
+    int Id,
+    string Name,
+    string Genre,
+    decimal Price,
+    DateTime ReleaseDate,
+    string ImageUrl
+);
+
+public record CreateGameDto(
+    [Required][StringLength(50)] string Name,
+    [Required][StringLength(20)] string Genre,
+    [Range(1, 100)] decimal Price,
+    DateTime ReleaseDate,
+    [Url][StringLength(100)] string ImageUrl
+);
+
+public record UpdateGameDto(
+    [Required][StringLength(50)] string Name,
+    [Required][StringLength(20)] string Genre,
+    [Range(1, 100)] decimal Price,
+    DateTime ReleaseDate,
+    [Url][StringLength(100)] string ImageUrl
+);
