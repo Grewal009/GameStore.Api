4ea85e6e10945848e8f82777d863068ffc725979
Aneel Grewal <aneel.grewal@hotmail.com>
2024-09-14 23:06:13 +0200

    added configuration to appsettings.json and read in Program.cs file

--- Program.cs
+++ Program.cs
@@ -6,6 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 //only one instance for the entire lifecycle
 builder.Services.AddSingleton<IGamesRepository, InMemoryGamesRepository>();
 
+var connString = builder.Configuration.GetConnectionString("GameStoreContext");
+
 var app = builder.Build();
 
 app.MapGamesEndpoints(); //using extension method
