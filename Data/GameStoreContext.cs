857c1e4ef9e56557aa126fde14d48287e5dbcf87
Aneel Grewal <aneel.grewal@hotmail.com>
2024-09-17 00:30:31 +0200

    using IEntityTypeConfiguration to tell sql server what precision and scale we want to use for Price

--- Data/GameStoreContext.cs
+++ Data/GameStoreContext.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using GameStore.Api.Entities;
 using Microsoft.EntityFrameworkCore;
 
@@ -10,5 +11,10 @@ namespace GameStore.Api.Data
         }
 
         public DbSet<Game> Games => Set<Game>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
     }
 }
