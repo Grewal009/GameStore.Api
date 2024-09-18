0bebb857275f3bd1e81440393113056e81dbb655
Aneel Grewal <aneel.grewal@hotmail.com>
2024-09-18 19:12:58 +0200

    Applying migrations on startup of application in Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using GameStore.Api.Data;
 using GameStore.Api.Endpoints;
 using GameStore.Api.Repositories;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,10 +10,15 @@ builder.Services.AddSingleton<IGamesRepository, InMemoryGamesRepository>();
 
 // reading connection string from user-secrets
 var connString = builder.Configuration.GetConnectionString("GameStoreContext");
-
 builder.Services.AddSqlServer<GameStoreContext>(connString);
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<GameStoreContext>();
+    dbContext.Database.Migrate();
+}
 
 app.MapGamesEndpoints(); //using extension method
 
