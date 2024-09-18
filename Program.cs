be834f11f59b725908ad3de8a76eb5013da18e53
Aneel Grewal <aneel.grewal@hotmail.com>
2024-09-18 18:30:40 +0200

    applying migrations into sql server instance

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 //only one instance for the entire lifecycle
 builder.Services.AddSingleton<IGamesRepository, InMemoryGamesRepository>();
 
+// reading connection string from user-secrets
 var connString = builder.Configuration.GetConnectionString("GameStoreContext");
 
 builder.Services.AddSqlServer<GameStoreContext>(connString);
