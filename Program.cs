ff22603b6ea456ce11bdf303c2112908c9487783
Aneel Grewal <aneel.grewal@hotmail.com>
2024-09-20 22:52:54 +0200

    using EntityFrameworkGamesRepository instead of InMemoryGamesRepository

--- Program.cs
+++ Program.cs
@@ -1,12 +1,16 @@
 using GameStore.Api.Data;
 using GameStore.Api.Endpoints;
+using GameStore.Api.Entities;
 using GameStore.Api.Repositories;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 //only one instance for the entire lifecycle
-builder.Services.AddSingleton<IGamesRepository, InMemoryGamesRepository>();
+//builder.Services.AddSingleton<IGamesRepository, InMemoryGamesRepository>();
+
+//using EntityFrameworkGamesRepository instead of InMemoryGamesRepository
+builder.Services.AddScoped<IGamesRepository, EntityFrameworkGamesRepository>();
 
 // reading connection string from user-secrets
 var connString = builder.Configuration.GetConnectionString("GameStoreContext");
