7e9a3f71f8c640b0dafcbca3e1385d1ece521caf
Aneel Grewal <aneel.grewal@hotmail.com>
2024-09-22 00:35:05 +0200

    making InitializeDbAsync in DataExtensions to follow async programmering model and update Program.cs file

--- Program.cs
+++ Program.cs
@@ -18,7 +18,7 @@ builder.Services.AddRepositories(builder.Configuration);
 
 var app = builder.Build();
 
-app.Services.InitializeDb();
+await app.Services.InitializeDbAsync();
 
 app.MapGamesEndpoints(); //using extension method
 
