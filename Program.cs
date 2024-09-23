df2291e25fa93eb5519c99be5dbb06f98367f0e9
Aneel Grewal <aneel.grewal@hotmail.com>
2024-09-23 23:17:09 +0200

    added nuget package for Authentication.JwtBearer & tried to access endpoints, get 401 Unauthorised status on all the endpoints except get all the games

--- Program.cs
+++ Program.cs
@@ -16,6 +16,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRepositories(builder.Configuration);
 
+builder.Services.AddAuthentication().AddJwtBearer();
+
 builder.Services.AddAuthorization();
 
 var app = builder.Build();
